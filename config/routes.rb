aa802fcc5f80363401f1841c0edeb95b61cb56cf
Jimmy <pham.duy.dy@moneyforward.vn>
2022-05-05 18:17:51 +0700

    Guest shortern url

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :shorted_links
+      post 'public_shorted_links', to: 'public_shorted_links#create'
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks] # add this and overide the omniauth callbacks
       post 'social_auth/callback', to: 'social_auth#authenticate_social_auth_user' # this is the line where we add our routes
     end
